e1a5321365ebb08c794d12c7acfa1a42444bfe93
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-03-24 21:04:34 -0400

    Been working on function lessons in TOP

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+        function add7(num) {
+             return num + 7
+        }
+
+        console.log(add7(1));
+
+        function multiply(num) {
+            return num * 2
+        }
+
+        console.log(multiply(4))
+
+        function capitalize(str) {
+            
+        }
+
+        
+        function lastletter() {
+            
+        }
